b430034154890cff3fd7c90eb6d6943a5cb02e5b
Ryan Anderson <rustyoldrake@gmail.com>
2019-04-16 17:55:13 -0700

    Update step2_filter_wav.py

--- step2_filter_wav.py
+++ step2_filter_wav.py
@@ -12,15 +12,12 @@ from scipy.fftpack import fft, fftfreq
 from pydub import AudioSegment
 
 wav_filename = "square.wav"
-
 samplerate, data = wavfile.read(wav_filename)
 
 total_samples = len(data)
-
 limit = int((total_samples /2)-1)
 
 fft_abs = abs(fft(data))
-
 freqs = fftfreq(total_samples,1/samplerate)
 
 # plot the frequencies
@@ -30,6 +27,21 @@ freqs = fftfreq(total_samples,1/samplerate)
 # plt.ylabel('amplitude')
 # plt.show()
 
-####### square wave to A PLOT (NOTE X SCALE IS GOOFY HERE - ILLUSTRATIVE ONLY) - works*
-plt.plot(freqs[:limit], fft_abs[:limit])
-plt.savefig('square_fft.png')
+####### FFT PLOT - PNG File - works
+plt.plot(freqs[:limit], fft_abs[:limit]) # comment out if you dont want both before and after to display
+#plt.savefig('square_fft.png')
+
+
+## LOW PASS BUTTERWORTH FILTER  - works
+## https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.lfilter.html
+
+# Create an order 3 lowpass butterworth filter
+b, a = signal.butter(3, 0.05)
+
+# REPEAT ABOVE ON FILTERED DATA
+# Use filtfilt to apply the filter:
+data_filtered = signal.filtfilt(b, a, data)
+fft_abs_filtered = abs(fft(data_filtered))
+freqs_filtered = fftfreq(total_samples,1/samplerate)
+plt.plot(freqs_filtered[:limit], fft_abs_filtered[:limit])
+plt.savefig('square_fft_filtered.png')
